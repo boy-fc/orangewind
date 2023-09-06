20ec62f04caf38f9403aee3fa95994f557515eae
fengc6 <fengc6@ziroom.com>
2023-09-06 17:23:36 +0800

    feat：代码优化

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -173,15 +173,6 @@ module.exports = {
         }
       }
     ],
-    [ 
-      "vuepress-plugin-nuggets-style-copy", 
-      {
-        copyText: "复制代码",
-        tip: {
-          content: "复制成功"
-        }
-      }
-    ],
     // 菜单高亮
     [ 
       '@vuepress/active-header-links',
