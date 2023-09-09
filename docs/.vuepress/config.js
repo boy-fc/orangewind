d81afc233378ddff8c3fb1b32d0430643021a936
fengc6 <fengc6@ziroom.com>
2023-09-09 21:12:46 +0800

    feat：config修改

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -103,11 +103,10 @@ module.exports = {
         link: 'https://www.recoluan.com'
       }
     ],
-    // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
-    repo: 'https://github.com/boy-fc/orangewind',
-    // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
-    // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
-    repoLabel: '查看源码',
+    // 顶部导航-查看源码
+    // repo: 'https://github.com/boy-fc/orangewind',
+    // 顶部导航-查看源码
+    // repoLabel: '查看源码',
     // 假如你的文档仓库和项目本身不在一个仓库：
     docsRepo: 'https://github.com/boy-fc/orangewind/',
     // 假如文档不是放在仓库的根目录下：
